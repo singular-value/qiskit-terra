8bbd0914717ccb5851f5e4169d00f58a153e0f63
Pranav Gokhale <pranavgokhale@uchicago.edu>
2019-11-23 21:14:48 -0600

    Almaden-specific hack to get ZZ interaction when CR is in the 'wrong' direction

--- qiskit/extensions/standard/zz_interaction.py
+++ qiskit/extensions/standard/zz_interaction.py
@@ -53,7 +53,27 @@ class ZZInteractionGate(Gate):
 
 
 def zz_interaction(self, theta, q1, q2):
-    return self.append(ZZInteractionGate(theta), [q1, q2], [])
+    # NOTE: CR is implemented "one way" between each connected qubit pair.
+    # ZZ interaction tries to do CR with q1 as control and q2 as control. However, if
+    # we have the "wrong" direction, there is a simple fix: just flip our treatment of q1 and q2.
+    # This only works because the ZZ interaction unitary is diagonal, so it is insensitive
+    # to a flipping of the treatment of q1 and q2. The map of cr directions is obtained as:
+    # cr_map = set()
+    # for i, j in config.coupling_map:
+    #     duration_ij = cmd_def.get('cx', qubits=[i, j]).duration
+    #     duration_ji = cmd_def.get('cx', qubits=[j, i]).duration
+    #     # shorter pulse is the one with the "native" CR direction
+    #     if duration_ij < duration_ji:
+    #         cr_map.add((i, j))
+    #     else:
+    #         cr_map.add((j, i))
+    # print(cr_map)
+    cr_map = {(11, 16), (10, 11), (5, 6), (2, 1), (8, 9), (1, 6), (14, 9), (16, 17), (19, 18), (6, 7), (12, 13), (16, 15), (18, 13), (12, 7), (18, 17), (2, 3), (8, 7), (11, 12), (0, 1), (10, 5), (13, 14), (3, 8), (4, 3)}
+
+    if (q1, q2) in cr_map:  # correct CR direction
+        return self.append(ZZInteractionGate(theta), [q1, q2], [])
+    else:  # incorrect CR, so just flip the treatment of the two qubits
+        return self.append(ZZInteractionGate(theta), [q2, q1], [])
 
 
 QuantumCircuit.zz_interaction = zz_interaction
